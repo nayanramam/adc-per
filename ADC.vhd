b898886f272a6f11c633b67b7e5b082dab154c20
Khalil <dkhalil8@ece-linlabsrv01.ece.gatech.edu>
2026-04-09 19:54:42 -0400

    free-running conversions, memory map update, controller instantiation

--- ADC.vhd
+++ ADC.vhd
@@ -1,35 +1,76 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.all;
 
--- TODO: Description goes here
-
 ENTITY ADC IS
 	PORT (
-        -- from SCOMP
-		io_addr	 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        io_read  : IN STD_LOGIC;
-		resetn 	 : IN STD_LOGIC;
+        -- System signals
+		clk    : IN STD_LOGIC;
+		resetn : IN STD_LOGIC;
+		io_read : IN STD_LOGIC;
 
-        -- to SCOMP
-        io_data  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-        io_en    : OUT STD_LOGIC
+        -- WRITE TO SCOMP
+		sel	 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        config  : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+
+		-- READ TO SCOMP
+        adc_data  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+
+		-- SPI to LTC2308 chip
+		sclk : OUT STD_LOGIC;
+		conv : OUT STD_LOGIC;
+		mosi : OUT STD_LOGIC;
+		miso : IN  STD_LOGIC
+
 	);
 END ADC;
 
 ARCHITECTURE arch OF ADC IS
-	TYPE MODE_TYPE IS ( sgl_endt, diff, ttl_debug, err );
+	-- SCOMP Communication Enum Types
+	TYPE MODE_TYPE IS ( sgl_end, diff, ttl_debug, err );
 	TYPE CHANNEL_TYPE IS ( ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch_error);
 	TYPE TTL_CONFIG IS ( ttl_input_0, ttl_input_1, ttl_output_0, ttl_output_1 );
-
+	
+	-- SCOMP Communication Signals	
 	SIGNAL channel  : CHANNEL_TYPE;
 	SIGNAL channel_neg : CHANNEL_TYPE;
 	SIGNAL io_mode  : MODE_TYPE;
 	SIGNAL ttl_config  : TTL_CONFIG;
 	
+	-- FSM for free-running conversions
+	TYPE ADC_STATE IS (IDLE, CONVERTING, WAIT_DONE, DONE);
+	SIGNAL state      : ADC_STATE;
+	SIGNAL result_reg : STD_LOGIC_VECTOR(11 DOWNTO 0);
+
+	-- ADC Info Signals
+	SIGNAL adc_busy   : STD_LOGIC;
+	SIGNAL adc_start  : STD_LOGIC;
+	SIGNAL adc_result : STD_LOGIC_VECTOR(11 DOWNTO 0);
+
 BEGIN
 
-	-- Combinationally select channel based on io_addr
-	WITH io_addr(3 DOWNTO 0) SELECT
+	-- Instantiate the LTC2308 controller
+	ltc : ENTITY work.LTC2308_ctrl
+    
+	-- SPI Clock set to 20 MHz
+	GENERIC MAP (
+        CLK_DIV => 1
+    )
+
+	-- Port mapping for the LTC2308 controller
+    PORT MAP (
+        clk     => clk,
+        nrst    => resetn,
+        start   => adc_start,
+        rx_data => adc_result,
+        busy    => adc_busy,
+        sclk    => sclk,
+        conv    => conv,
+        mosi    => mosi,
+        miso    => miso
+    );
+
+	-- Combinationally select channel based on sel
+	WITH sel(3 DOWNTO 0) SELECT
 		channel <= ch0 WHEN "0000",
 		           ch1 WHEN "0001",
 		           ch2 WHEN "0010",
@@ -39,9 +80,17 @@ BEGIN
 		           ch6 WHEN "0110",
 		           ch7 WHEN "0111",
 		           ch_error WHEN OTHERS;
-	
-	WITH io_addr(10 DOWNTO 8) SELECT
-	channel_neg <= ch0 WHEN "0000",
+
+	-- Combinationally select mode based on sel			   
+	WITH sel(5 DOWNTO 4) SELECT
+		io_mode <=  sgl_end    WHEN "00",
+					diff       WHEN "01",
+		            ttl_debug  WHEN "10",
+		            err    WHEN OTHERS;
+
+	-- Combinationally select negative channel based on config			   
+	WITH config(3 DOWNTO 0) SELECT
+		channel_neg <= ch0 WHEN "0000",
 				ch1 WHEN "0001",
 				ch2 WHEN "0010",
 				ch3 WHEN "0011",
@@ -51,23 +100,50 @@ BEGIN
 				ch7 WHEN "0111",
 				ch_error WHEN OTHERS;
 
-	WITH io_addr(5 DOWNTO 4) SELECT
-		io_mode <=  sgl_end    WHEN "00",
-					diff       WHEN "01",
-		            ttl_debug  WHEN "10",
-		            err    WHEN OTHERS;
-
-	WITH io_addr(13 DOWNTO 12) SELECT
+	-- Combinationally ttl configuration based on config			   
+	WITH config(5 DOWNTO 4) SELECT
 		ttl_config <=   ttl_input_0   WHEN "00",
 					ttl_output_0       WHEN "01",
 		            ttl_input_1  WHEN "10",
 		            ttl_output_1    WHEN "11";
 
 	
-	-- Process block to update display memory, which in turn updates display output
-	PROCESS (io_read, resetn)
+	-- Drive adc_data: zero-pad in single-ended mode, sign-extend otherwise
+	adc_data <= "0000" & result_reg WHEN io_mode = sgl_end ELSE
+	            (15 DOWNTO 12 => result_reg(11)) & result_reg;
+
+	-- Free-running ADC conversion state machine
+	PROCESS (clk, resetn)
 	BEGIN
-		
+		IF resetn = '0' THEN
+			state     <= IDLE;
+			adc_start <= '0';
+			result_reg <= (OTHERS => '0');
+
+		ELSIF rising_edge(clk) THEN
+			CASE state IS
+
+				WHEN IDLE =>
+					adc_start <= '1';
+					state     <= CONVERTING;
+
+				WHEN CONVERTING =>
+					adc_start <= '0';
+					IF adc_busy = '1' THEN
+						state <= WAIT_DONE;
+					END IF;
+
+				WHEN WAIT_DONE =>
+					IF adc_busy = '0' THEN
+						state <= DONE;
+					END IF;
+
+				WHEN DONE =>
+					result_reg <= adc_result;
+					state      <= IDLE;
+
+			END CASE;
+		END IF;
 	END PROCESS;
 	
 END arch;
