5a6d8fc9e78ff367bdb088396fc7305d9b940231
tmochida3 <tomohiro.mochidajp@gmail.com>
2026-04-14 16:56:19 -0400

    new test file for single ended

---
+++ test_singled_ended.asm
@@ -0,0 +1,57 @@
+; Register map:
+;   [1:0]  IO_MODE       00=single, 01=diff, 10=ttl, 11=err
+;   [4:2]  IO_ADDR_POS   positive channel (0-7)
+;   [7:5]  IO_ADDR_NEG   negative channel (0-7)
+;   [9:8]  TTL_CONFIG    sub-mode config
+;
+; IO address map (matched to game file):
+;   ADC    EQU 3 
+;   HEX_UP EQU 4 
+;   HEX_LO EQU 5 
+
+ORG 0
+
+; IO address constants
+ADC     EQU    3
+HEX_UP  EQU    4
+HEX_LO  EQU    5
+
+; LIVE VOLTAGE DISPLAY: CH0 continuous read
+; Reached only after all tests pass
+; Display shows live hex mV value: GND=0000, 3.3V=0CE4, 4.096V=1000
+LIVE_DISPLAY:
+    LOADI   &B0000000000000000
+    OUT     ADC
+    CALL    WAIT_SINGLE
+    IN      ADC
+    OUT     HEX_UP
+    CALL    DELAY
+    JUMP    LIVE_DISPLAY
+
+; SUBROUTINES 
+
+WAIT_SINGLE:
+    LOADI   200
+
+; Short delay to prevent display flicker in live mode
+DELAY:
+    LOAD    DELAY_COUNT
+    STORE   DELAY_TEMP
+
+
+; DATA & CONSTANTS
+HOLD_COUNT:     DW 20000
+DELAY_COUNT:    DW 10000
+HOLD_TEMP:      DW &B0000000000000000
+DELAY_TEMP:     DW &B0000000000000000
+DEAD_CONST:     DW &B1101111010101101  ; 0xDEAD
+FAIL_CONST:     DW &B1111111111111111  ; 0xFFFF - shown if error test fails
+CD_TEMP:        DW &B0000000000000000
+
+; Results
+RESULT_SGL_CH0: DW &B0000000000000000
+RESULT_SGL_CH1: DW &B0000000000000000
+RESULT_SGL_CH7: DW &B0000000000000000
+RESULT_DIFF:    DW &B0000000000000000
+RESULT_TTL:     DW &B0000000000000000
+RESULT_ERR:     DW &B0000000000000000
