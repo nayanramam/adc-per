b779d2c672ba66b34b5478754e225885f19c3721
danjkhalil-creator <danjkhalil@gmail.com>
2026-04-17 17:54:53 -0400

    cleanup

--- differential_demo.asm
+++ differential_demo.asm
@@ -1,12 +1,10 @@
-; Differential mode bare-bones loop
-; CH0(+) vs CH1(-), result shown on HEX_UP
-;
-; Config word: &B0000000001000001
-;   [1:0] = 01  -> differential mode
-;   [4:2] = 000 -> CH0 positive
-;   [7:5] = 001 -> CH1 negative
+; [1:0] = IO Mode
+; [4:2] = Channel Address Pos
+; [7:5] = Channel Address Neg
+; [9:8] = TTL Configuration
+; Config word: &B XXXXXX XX 010 000 01
 
-    ORG     0
+ORG     0
 
 ADC     EQU       3
 HEX_RIGHT  EQU    4
